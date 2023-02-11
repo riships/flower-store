0439b4f9f98cdc5ce0f158c4657bcf162da71ab2
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-02-11 10:54:39 +0000

    Navbar is ready

--- src/conponents/Navbar.jsx
+++ src/conponents/Navbar.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
