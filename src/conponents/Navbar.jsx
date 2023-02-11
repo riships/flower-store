fca7df1c2a21893b7ab1a11c02caadf09ecc9ec4
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-02-11 11:20:48 +0000

    Navbar With var is designed

--- src/conponents/Navbar.jsx
+++ src/conponents/Navbar.jsx
@@ -2,32 +2,52 @@ import React from "react";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
+import Work from "./Work";
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 
 
 function Navbar() {
+    const menuItems = [
+        {
+            text: "Home",
+            link: "/",
+        },
+        {
+            text: "About",
+            link: "/about",
+        },
+        {
+            text: "Contact",
+            link: "/contact",
+        },
+        {
+            text: "Work",
+            link: "/work"
+        }
+    ];
     return (
+        <>
 
-        <BrowserRouter>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/contact' element={<Contact />} />
-            </Routes>
-        </BrowserRouter>
+            <BrowserRouter>
+
+                <nav>
+                    <ul>
+                        {menuItems.map((menuItem, index) => (
+                            <li key={index}>
+                                <Link to={menuItem.link}>{menuItem.text}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/work' element={<Work />} />
+                </Routes>
+
+            </BrowserRouter>
+        </>
     );
 };
 
