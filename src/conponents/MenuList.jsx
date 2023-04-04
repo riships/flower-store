883c64dd2c78e791d4ea45b650f6a4c70725349b
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-04-04 07:05:38 +0000

    Menu Added

--- src/conponents/MenuList.jsx
+++ src/conponents/MenuList.jsx
@@ -3,11 +3,25 @@ import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
 import Work from "./Work";
-import { Navbar, Container, Nav } from 'react-bootstrap';
+// import { slide as Menu } from 'react-burger-menu'
+// import { Navbar, Container, Nav } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+// import { slide as Menu } from 'react-burger-menu'
+import { useState } from 'react'
+import '../stylesheets/style.css';
+import Singleproduct from "./Singleproduct";
+import styled from 'styled-components';
+
+
+const Wrapper = styled.nav``;
 
 
 function MenuList() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     const menuItems = [
         {
             text: "Home",
@@ -24,36 +38,59 @@ function MenuList() {
         {
             text: "Work",
             link: "/work"
+        }, {
+            text: "Signle Product",
+            link: "/singleproduct/:id"
         }
+
     ];
     return (
         <>
 
             <BrowserRouter>
-                <Navbar bg="light" variant="light" expand="lg" >
-                    <Container>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                {menuItems.map((menuItem, index) => (
-                                    <div key={index}>
-                                        <Link to={menuItem.link}>{menuItem.text}</Link>
-                                    </div>
-                                ))}
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar >
+                <Wrapper className="hamburger-menu hdr_menu">
+                    {/* <input name="toggle" type="checkbox" id="menu_toggle" />
+                    <label for="menu_toggle" class="menu_btn">
+                        <span></span>
+                    </label>
+                    <div className="menu_box">
+                        {menuItems.map((menuItem, index) => (
+
+                            <div className="mnu_link" key={index}>
+                                <Link to={menuItem.link}>{menuItem.text}</Link>
+                            </div>
+
+                        ))}
+                    </div> */}
+
+                    <label onClick={toggleMenu} for="menu_toggle" className="menu_btn">
+                        <span></span>
+                    </label>
+                    {isOpen && (
+                        <div className="menu_box">
+                            {menuItems.map((menuItem, index) => (
+
+                                <div className="mnu_link" key={index}>
+                                    <Link to={menuItem.link}>{menuItem.text}</Link>
+                                </div>
+
+                            ))}
+                        </div>
+                    )}
+
+                </Wrapper>
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/contact' element={<Contact />} />
                     <Route path='/work' element={<Work />} />
+                    <Route path="/singleproduct/:id" element={<Singleproduct />} />
                 </Routes>
 
             </BrowserRouter >
         </>
     );
+
 };
 
 export default MenuList;
