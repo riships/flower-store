cd706d8c0c068a23b180cad924f52d28f9f0ffca
riships <avviare.rishi@gmail.com>
2023-04-30 13:26:20 +0530

    keys

--- src/conponents/FlowerType.jsx
+++ src/conponents/FlowerType.jsx
@@ -3,13 +3,18 @@ import { useProductFlowers } from '../conponents/context/productcontext'
 import '../stylesheets/product.css';
 import { Link } from "react-router-dom";
 // import FormatPrice from "../Helper/FormatPrice";
+import Rating from 'react-rating';
 
 
 function FlowerType() {
+    const [rating, setRating] = useState(0);
     const { isLoading, flowerType } = useProductFlowers();
     console.log(flowerType);
     if (isLoading) {
         return <div>Loading...</div>;
+    }
+    function handleRating(value) {
+        setRating(value);
     }
     return (
         <div>
@@ -19,22 +24,22 @@ function FlowerType() {
                     return (
                         <div key={index} className="product-card">
                             <Link to={`/singleproduct/${flower.id}`}>
-                                <img src={flower.image} alt={flower.category} className="product-image" />
+                                <img src={flower.images[0]} alt={flower.category} className="product-image" />
                             </Link>
                             <div className="product-details">
                                 <div className="nm-and-prc">
                                     <h2 className="product-name">{flower.name}</h2>
-                                    <h2 className="product-price">{flower.scientific_name}</h2>
+                                    <h2 className="product-price">{flower.sub_category}</h2>
                                 </div>
 
-                                <p className="product-dis">{flower.description.slice(0, 100)}</p>
-                                {/* <div className="product-rating">
-                                    {Array.from(Array(rating)).map((_, i) => (
+                                <p className="product-dis">{flower.description}</p>
+                                <div className="product-rating">
+                                    {Array.from(Array(flower.ratings)).map((_, i) => (
                                         <span key={i} className="star">
                                             ★
                                         </span>
                                     ))}
-                                </div> */}
+                                </div>
                                 <button className="product-button">Add to Cart</button>
                             </div>
                         </div>
