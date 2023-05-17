9ea4fa6ab11ddc54747880c578c40108a6f444de
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-05-17 15:48:37 +0530

    Nothinng

--- src/conponents/Cart.jsx
+++ src/conponents/Cart.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 // import AddToCart from './AddToCart'
+import styled from 'styled-components';
+
+const CartContainer = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+// Create a styled component for the cart item
+const CartItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+
+  img {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    margin-right: 20px;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
+
 
 function Cart() {
     return (
