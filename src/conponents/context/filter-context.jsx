f91a45f8abcc338bec5e6b615c1bc75e40214faa
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-05-19 17:20:02 +0530

    context used for filter the data of all products

--- src/conponents/context/filter-context.jsx
+++ src/conponents/context/filter-context.jsx
@@ -1,8 +1,25 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 import { useProductFlowers } from "./productcontext";
+import reducer from "../reducers/filterReducers";
 const FilterContext = createContext();
+const initialState = {
+    filter_flowers: [],
+    all_flowers: [],
+}
 
 export const FilterContextProvider = ({ children }) => {
+    const { flowers } = useProductFlowers();
+    // console.log(flowers);
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        dispatch({
+            type: "LOAD_FILTER_FLOWERS",
+            payload: flowers
+        });
+    }, [])
+
     return (
         <FilterContext.Provider value={{ ...state }}>
             {children}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './conponents/context/productcontext';
-import { FilterContextProvider } from './conponents/filter-context';
+import { FilterContextProvider } from './conponents/context/filter-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
