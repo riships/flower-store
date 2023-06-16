619ff0220f0688abab476d65f7cc1c8c89d75998
riships <avviare.rishi@gmail.com>
2023-06-16 15:18:03 +0530

    package file updated

--- src/conponents/ListView.jsx
+++ src/conponents/ListView.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Row, CardImg, Card, CardText, Col, CardTitle } from 'reactstrap';
+// import { Row, CardImg, Card, CardText, Col, CardTitle } from 'reactstrap';
 import { Link } from "react-router-dom";
+import StarRating from "./StarRating"
+import '../stylesheets/list-view.css'
+import FormatPrice from '../Helper/FormatPrice';
 
 
 
@@ -8,26 +11,58 @@ function ListView({ product }) {
     return (<div>
         {
             product.map((curEle) => {
-                const { images, id, name, description } = curEle;
-                return <Card className="mb-3" key={id} style={{ maxWidth: '540px' }}>
-                    <Row className="g-0">
-                        <Col md={4}>
-                            <Link to={`/singleproduct/${id}`}>
-                                <CardImg src={images[0].url} alt={name} />
-                            </Link>
-                        </Col>
-                        <Col md={8}>
-                            <Card>
-                                <CardTitle>{name}</CardTitle>
-                                <CardText>{description}
-                                </CardText>
-                                <CardText>
-                                    <small className="text-medium-emphasis">Last updated 3 mins ago</small>
-                                </CardText>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Card>
+                const { images, id, name, description, ratings, price, sub_category } = curEle;
+                return <div className="container" key={id}>
+                    <div className="col-xs-12 col-md-6 bootstrap snippets bootdeys" >
+                        <div className="product-content product-wrap clearfix">
+                            <div className="row">
+                                <div className="col-md-5 col-sm-12 col-xs-12">
+                                    <div className="product-image">
+                                        <Link to={`/singleproduct/${id}`}>
+                                            <img src={images[0].url} alt={name} className="img-responsive" />
+                                        </Link>
+                                        <span className="tag2 hot">
+                                            Beautiful
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="col-md-7 col-sm-12 col-xs-12">
+                                    <div className="product-deatil">
+                                        <h5 className="name">
+                                            <Link to={`/singleproduct/${id}`}>
+                                                {name} <span>{sub_category}</span>
+                                            </Link>
+                                        </h5>
+                                        <p className="price-container">
+                                            <span>
+                                                <FormatPrice price={price * 200} />
+                                            </span>
+                                        </p>
+                                        <span className="tag1"></span>
+                                    </div>
+                                    <div className="description">
+                                        <p>{description}</p>
+                                    </div>
+                                    <div className="product-info smart-form">
+                                        <div className="row">
+                                            <div className="col-md-6 col-sm-6 col-xs-6">
+                                                <Link to={`/cart`} className='btn btn-success'>
+                                                    Add to cart
+                                                </Link>
+                                            </div>
+                                            <div className="col-md-6 col-sm-6 col-xs-6">
+                                                <div className="rating">
+                                                    <StarRating className="str-color" star={ratings} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             })
         }
     </div>
