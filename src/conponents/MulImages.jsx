6ee5944b4763aa20570676c4887df21011b24b38
riships <avviare.rishi@gmail.com>
2023-06-20 12:37:56 +0530

    cursol added

--- src/conponents/MulImages.jsx
+++ src/conponents/MulImages.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import '../stylesheets/mulimages.css';
 import '../stylesheets/style.css';
 import styled from 'styled-components';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MulImages({ imags = [{ "url": '' }] }) {
     // console.log(imags);
@@ -10,25 +12,38 @@ function MulImages({ imags = [{ "url": '' }] }) {
         <div className="multple-imgs">
             <img src={mainImge.url} alt="product-img" className='prdct--img' />
         </div>
-        <div className="grid2 grid-four-column">
+        {/* <div className="grid2 grid-four-column"> */}
+        <Carousel
+            showArrows={false}
+            // showIndicators={false}
+            showStatus={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            stopOnHover={false}
+            centerSlidePercentage={50}
+            swipeable={true}
+
+        >
             {
                 imags.map((curElm, index) => {
                     return (
-                        <ImgMul>
-                            <figure key={index}>
-                                <img src={curElm.url} alt='product-img' className='singl--prdct--img' onClick={() => setMainImge(curElm)} />
-                            </figure>
+                        <ImgMul onClick={() => setMainImge(curElm)} key={index}>
+                            <img src={curElm.url} alt='product-img' className='singl--prdct--img' />
                         </ImgMul>
+
                     )
                 })
             }
-        </div>
+        </Carousel>
+        {/* </div> */}
     </>
     )
 };
 const ImgMul = styled.div`
-    height:65px;
-    width:100px;
+    margin-top:15px;
+    margin-bottom:15px;
+    height:130px;
+    width:130px;
     overflow:hidden;
 `
 
