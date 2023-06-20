91e9f111d562d3c96b45a5561b9b4f927f6f7984
riships <avviare.rishi@gmail.com>
2023-06-20 16:01:07 +0530

    sursol with popup

--- src/conponents/MulImages.jsx
+++ src/conponents/MulImages.jsx
@@ -4,13 +4,24 @@ import '../stylesheets/style.css';
 import styled from 'styled-components';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ReactModal from 'react-modal';
 
 function MulImages({ imags = [{ "url": '' }] }) {
     // console.log(imags);
     const [mainImge, setMainImge] = useState(imags[0]);
+    const [isOpen, setIsOpen] = useState(false);
     return (<>
+        <ReactModal
+            appElement={document.getElementById('root')}
+            className="RectModal"
+            isOpen={isOpen}
+            onRequestClose={() => setIsOpen(false)}
+        // overlayClassName="custom-overlay-styles"
+        >
+            <img src={mainImge.url} alt="product-img" className='prdct--img--full--view' />
+        </ReactModal>
         <div className="multple-imgs">
-            <img src={mainImge.url} alt="product-img" className='prdct--img' />
+            <img onClick={() => setIsOpen(true)} src={mainImge.url} alt="product-img" className='prdct--img' />
         </div>
         {/* <div className="grid2 grid-four-column"> */}
         <Carousel
@@ -31,7 +42,6 @@ function MulImages({ imags = [{ "url": '' }] }) {
                         <ImgMul onClick={() => setMainImge(curElm)} key={index}>
                             <img src={curElm.url} alt='product-img' className='singl--prdct--img' />
                         </ImgMul>
-
                     )
                 })
             }
