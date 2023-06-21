d1c9c2908af64fa04377d5b8240809cbb41b4030
riships <avviare.rishi@gmail.com>
2023-06-21 17:10:12 +0530

    search bar designing

--- src/conponents/FilterSection.jsx
+++ src/conponents/FilterSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useFilterContext } from './context/filter-context'
+import '../stylesheets/filterSection.css'
 
 function FilterSection() {
     const { updateFilterValue, filters: { text }, } = useFilterContext();
@@ -9,10 +10,12 @@ function FilterSection() {
             <div className='filter--search'>
                 <form onSubmit={(e) => e.preventDefault()}>
                     <input
+                        className='search--input'
                         type="text"
                         name="text"
                         value={text}
                         onChange={updateFilterValue}
+                        placeholder='SEARCH'
                     />
                 </form>
             </div>
