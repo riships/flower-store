0a36e555793bda5a3a988f5ae4d45021b140afbe
riships <avviare.rishi@gmail.com>
2023-07-11 16:56:44 +0530

    error

--- src/conponents/FilterSection.jsx
+++ src/conponents/FilterSection.jsx
@@ -12,10 +12,10 @@ function FilterSection() {
 
 
     // TO GET THE UNIQUE DATA OF EACH FIELD
-    const getUniqueData = (data, property) => {
-        let new 
+    // const getUniqueData = (data, property) => {
+    //     let new 
 
-    }
+    // }
 
 
     //WE NEED UNIQUE DATA
